c2bd387ab5cdb7ac1ca7b854ebb96022172643c4
Baharudin Yusuf <b.yusuf.w@gmail.com>
2024-08-22 20:13:56 +0700

    Tes Email Notification Berhasil menggunakan queue dan jobs

---
+++ resources/views/emails/test_mail.blade.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Send email via SMTP Gmail</title>
+</head>
+<body>
+
+<h2>Berhasil kirim email</h2>
+<div>
+    Ini juga
+</div>
+
+
+</body>
+</html>
